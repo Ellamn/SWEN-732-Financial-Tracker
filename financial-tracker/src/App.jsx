265ad2a5c5a9546abb792729bac644e29ac71c37
Ella Natter <emcnatter@gmail.com>
2026-02-19 12:48:53 -0500

    react setup?

---
+++ financial-tracker/src/App.jsx
@@ -0,0 +1,18 @@
+import trackerLogo from '/dollar-svgrepo-com.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={trackerLogo} className="logo" alt="tracker logo" />
+        </a>
+      </div>
+      <h1>Financial Tracker</h1>
+    </>
+  )
+}
+
+export default App
